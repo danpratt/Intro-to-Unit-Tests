9af4a7fc08ffb5b62c25e05d64645ea8a8ac206a
Daniel Pratt <daniel@blaumagier.com>
2017-08-15 12:49:11 -0700

    feat: added some functions and tested them
    - added return even true false
    - added fibinocci sequence function
    - added devslopes bug function
    - added tests for these functions
    
    - all tests passing

--- Intro to Unit Tests/ViewController.swift
+++ Intro to Unit Tests/ViewController.swift
@@ -15,11 +15,33 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func isNumberEven(number: Int) -> Bool {
+        return number % 2 == 0
     }
-
-
+    
+    func sumEvenValuedNumbersFibonacciSequence(limit: Int) -> Int {
+        var sum = 0
+        
+        var a = 1
+        var b = 1
+        
+        while b < limit {
+            if b % 2 == 0 {
+                sum += b
+            }
+            let h = a + b
+            a = b
+            b = h
+        }
+        
+        return sum
+    }
+    
+    func getLessonCoinAmount(coursePrice: Int) -> Int? {
+        let lessonRewardCoinAmountPercentage = 1
+        let decimal = Double(lessonRewardCoinAmountPercentage) / 100.0
+        return Int(Double(coursePrice) * decimal)
+    }
+    
 }
 
---
+++ Intro to Unit TestsTests/ViewControllerTests.swift
@@ -0,0 +1,61 @@
+//
+//  ViewControllerTests.swift
+//  Intro to Unit Tests
+//
+//  Created by Daniel Pratt on 8/15/17.
+//  Copyright © 2017 Daniel Pratt. All rights reserved.
+//
+
+import XCTest
+@testable import Intro_to_Unit_Tests
+
+class ViewControllerTests: XCTestCase {
+    
+    let sut: ViewController = ViewController()
+    let oddNumbers = [1, 3, 5, 7, 9]
+    let evenNumbers = [2, 4, 6, 8, 10]
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func test_isNumberEven_odd_false() {
+        
+        for i in oddNumbers {
+            XCTAssertFalse(sut.isNumberEven(number: i))
+        }
+        
+    }
+    
+    func test_isNumberEven_even_true() {
+        for i in evenNumbers {
+            XCTAssertTrue(sut.isNumberEven(number: i))
+        }
+    }
+    
+    func test_sumEvenValuedNumbersFibonacciSequence () {
+        
+        let limit = 4000000
+        let answer = 4613732
+        
+        let funcAnswer = sut.sumEvenValuedNumbersFibonacciSequence(limit: limit)
+        
+        XCTAssertEqual(funcAnswer, answer, "Expeted answer: 4613732")
+    }
+    
+    func test_CoinRewardedAmount_price100_1CoinEarned () {
+        let price = 100
+        let coinsEarned = 1
+        
+        XCTAssertEqual(sut.getLessonCoinAmount(coursePrice: price), coinsEarned)
+    }
+    
+}
